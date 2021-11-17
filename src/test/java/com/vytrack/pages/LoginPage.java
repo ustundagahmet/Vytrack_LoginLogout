8cdb628494f2039a46388345e5a1efdd1eb56483
Üstündag Ahmet <neustundagahmet@gmail.com>
2021-11-17 12:21:07 +0100

    to be added LoginPage code

--- src/test/java/com/vytrack/pages/LoginPage.java
+++ src/test/java/com/vytrack/pages/LoginPage.java
@@ -1,4 +1,28 @@
 package com.vytrack.pages;
 
+import com.vytrack.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class LoginPage {
+
+    public LoginPage(){
+        PageFactory.initElements(Driver.get(), this);
+    }
+
+    @FindBy(id = "prependedInput")
+    public WebElement usernameBox;
+
+    @FindBy(id = "prependedInput2")
+    public WebElement passwordBox;
+
+    @FindBy(id = "_submit")
+    public WebElement loginButton;
+
+    public void login(String userName, String password){
+        usernameBox.sendKeys(userName);
+        passwordBox.sendKeys(password);
+        loginButton.click();
+    }
 }
